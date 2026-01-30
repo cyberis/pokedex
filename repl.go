e4c5c59efef0462f5dbc3a84dccfdcc9f3e2b036
Cyberis <christopher.bortz@gmail.com>
2026-01-29 22:05:23 -0500

    Provide a Demo REPL

--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+		// For demonstration, just print the cleaned words
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
